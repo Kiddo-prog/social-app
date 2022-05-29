5496d488635b4dd7739fdb7faec4a1e7ba946d63
Kiddo-prog <husseinmubarak500@gmail.com>
2022-05-29 21:55:34 +0100

    Registered Profile Field

--- talkline/admin.py
+++ talkline/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Profile)
